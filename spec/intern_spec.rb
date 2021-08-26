27a2355d191531efae2ebb50216f162f7c879ea5
Desiree Lerma <desi.lerma96@yahoo.com>
2021-08-25 22:05:36 -0500

    Adds extra test for peer method.

--- spec/intern_spec.rb
+++ spec/intern_spec.rb
@@ -5,4 +5,9 @@ describe Intern do
         intern = Intern.new
         expect(intern.peer("my string")).to eq("my string")
     end
+
+    it 'should return the same string' do
+        intern = Intern.new
+        expect(intern.peer("this is working")).to eq("this is working")
+    end
 end
